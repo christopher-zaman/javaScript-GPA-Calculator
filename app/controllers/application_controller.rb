820d8850a1059c9f79e285fdd3904f5d000a3aab
czaman9 <christopher.zaman@baruchmail.cuny.edu>
2020-03-21 20:24:34 -0400

    Switch to Hello App

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,10 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  
+  def hello
+    render html: "¡hola, mundo!"
+  end
+  def bye
+    render html: "Goodbye World!"
+  end
+end
